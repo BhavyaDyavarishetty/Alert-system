88826c365ac3985beb3ac0258fe48f9d95995b18
Bhavya Dyavarishetty <bdyavarishetty@marketo.com>
2020-05-06 21:47:00 -0700

    initial commit

---
+++ src/test/java/com/covid19/alertsystem/AlertsystemApplicationTests.java
@@ -0,0 +1,11 @@
+package com.covid19.alertsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class AlertsystemApplicationTests {
+
+  @Test void contextLoads() {
+  }
+
+}
